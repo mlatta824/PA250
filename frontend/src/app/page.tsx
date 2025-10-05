83e31887b40cb0a56d2536df5a0ac03fa2ec3683
ajlgg <luthraa223@gmail.com>
2025-10-04 22:46:14 -0400

    I dont know

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -15,15 +15,12 @@ export default function Home() {
   };
 
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-indigo-600 text-white">
-      <div className="flex flex-col items-center justify-center text-center space-y-4">
-        <Header
-
-          headerText="Welcome to Name"
-          subtext="Look at the amazing history of our state light up!"
-        />
-        <Button onClick={handleGetStarted}>Get Started</Button>
-      </div>
+     <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600 text-white text-center px-4">
+      <Header headerText="Illuminating Our History"
+      subtext="Look at the amazing history of our state light up!" />
+      <Button onClick={handleGetStarted}>Get Started</Button>
     </main>
   );
 }
+
+
